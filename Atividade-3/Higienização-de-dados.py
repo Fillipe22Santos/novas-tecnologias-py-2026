11e26062fbde5869e56f00d2d7e43fc8018af532
fillipe22santos <yungzitoty@gmail.com>
2026-03-18 21:20:09 -0300

    ver 1 higiene

--- Atividade-3/Higienização-de-dados.py
+++ Atividade-3/Higienização-de-dados.py
@@ -1,13 +1,11 @@
 def limpar_dados(lista):
-
-    
     
     lista_limpa = []
 
     for pessoa in lista:
         nome = pessoa["nome"].strip().title()
         email = pessoa["email"].strip().lower()
-        status = pessoa["status_ativo"]  # já é booleano
+        status = pessoa["status_ativo"]  
 
         usuario_limpo = {
             "nome": nome,
@@ -18,3 +16,14 @@ def limpar_dados(lista):
         lista_limpa.append(usuario_limpo)
 
     return lista_limpa
+
+dados = [
+    
+{"nome": "Yuri Campos", "email": "yuri.campos@email.com", "stuatus_ativo": True},
+{"nome": "Aberth Cavalcanti","email": "alberth.cavalcanti@email.com","status_ativo": False},
+{"nome": "Matheus Ali","email": "matheus.ali@email.com","status_ativo": True},
+    
+    ]
+
+resultado = limpar_dados(dados)
+print(resultado)
